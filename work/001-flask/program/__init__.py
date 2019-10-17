5fcf10ea30582f9df597df1d5c8f2ebe9021bd5b
Tibor Leupold <tibor@lpld.io>
2019-10-17 11:30:02 -1000

    Added comment about cirular imports

--- work/001-flask/program/__init__.py
+++ work/001-flask/program/__init__.py
@@ -5,4 +5,9 @@ app = Flask(__name__)
 
 # This needs to be below the app instance!
 from program import routes
-
+# App is imported in the routes.py -- isn't this a circular dependency?
+# The routes are registered with the app in the routes.py.
+# Why do I need to include this here?
+# This is apparently to make the decorators for the routes work.
+# The circular import is directly addressed by the flask documentation:
+# https://flask.palletsprojects.com/en/1.1.x/patterns/packages/?highlight=Circ
