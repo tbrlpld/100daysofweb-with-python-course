7c11c43ae86c1af2ad1521235dae04bd04b000aa
Tibor Leupold <tibor@lpld.io>
2019-10-17 10:14:54 -1000

    Added simple template

--- work/001-flask/program/routes.py
+++ work/001-flask/program/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 
 # Flask is already imported in the __init__.py
 from program import app  # This is in the __init__.py
@@ -7,7 +8,7 @@ from program import app  # This is in the __init__.py
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello Pythonistas"
+    return render_template("index.html")
 
 
 @app.route("/hello/<string:name>")
