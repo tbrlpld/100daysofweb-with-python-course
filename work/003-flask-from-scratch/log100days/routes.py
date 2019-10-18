b19f4638eafb6bbff458c3eb2f55cfeb4ffc56bb
Tibor Leupold <tibor@lpld.io>
2019-10-18 11:01:52 -1000

    Added routes that return raw markdown from github

--- work/003-flask-from-scratch/log100days/routes.py
+++ work/003-flask-from-scratch/log100days/routes.py
@@ -1,3 +1,5 @@
+import requests
+
 from flask import render_template
 
 from log100days import app
@@ -8,3 +10,15 @@ from log100days import app
 @app.route("/index")
 def index():
     return render_template("home.html.j2")
+
+
+@app.route("/rules")
+def rules():
+    request = requests.get("https://raw.githubusercontent.com/tbrlpld/100-days-of-code/master/rules.md")
+    return request.content
+
+
+@app.route("/log")
+def log():
+    request = requests.get("https://raw.githubusercontent.com/tbrlpld/100-days-of-code/master/log.md")
+    return request.content
