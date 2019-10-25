55a08b65999a6a0fbd65bd6b0cec7cf0437212de
Tibor Leupold <tibor@lpld.io>
2019-10-24 20:05:57 -1000

    Created route to return list of cars

--- work/009-API-Star/app.py
+++ work/009-API-Star/app.py
@@ -3,26 +3,34 @@
 """Simple car data API."""
 
 import json
-from pprint import pprint
 
 from apistar import App, Route
+from apistar.http import JSONResponse
 
 
 def _load_car_data():
     with open("car_data.json") as car_data_file:
-        car_data = json.load(car_data_file)
+        car_data = json.load(car_data_file)[:12]
     return {car["id"]: car for car in car_data}
 
 
+# Global variable acts as a stand in for an actual database connection.
 cars = _load_car_data()
 
+STATUS_OK = 200
+STATUS_NOT_FOUND = 404
 
-def dummy():
-    return "hello world!"
+
+def get_cars() -> JSONResponse:
+    """Return all cars as in a sorted list."""
+    key_value_list = cars.items()
+    sorted_key_value_list = sorted(key_value_list)
+    list_of_car_dicts = [car[1] for car in sorted_key_value_list]
+    return JSONResponse(list_of_car_dicts, STATUS_OK)
 
 
 routes = [
-    Route("/", method="get", handler=dummy),
+    Route("/", method="get", handler=get_cars),
 ]
 
 app = App(routes=routes)
