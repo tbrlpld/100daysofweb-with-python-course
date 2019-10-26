33ccef928cdefcb056eec739ee51837d49d96861
Tibor Leupold <tibor@lpld.io>
2019-10-26 11:23:58 -1000

    Added test for read single user endpoint

--- work/011-api-star-people/test_app.py
+++ work/011-api-star-people/test_app.py
@@ -3,7 +3,8 @@
 """Tests for user API."""
 
 from http import HTTPStatus
-#
+
+import pytest
 from apistar import test
 #
 from app import app
@@ -24,3 +25,20 @@ def test_get_users():
     last_user = users[-1]
     assert last_user["userid"] == 1000
     assert last_user["username"] == "bokeevanrr"
+
+
+@pytest.mark.parametrize(("userid", "username"), [
+    (1, "hyurikov0"),
+    (1000, "bokeevanrr"),
+])
+def test_get_user(userid, username):
+    """Test get of one user by its id."""
+    response = client.get("/{0}".format(userid))
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()["username"] == username
+
+
+def test_get_user_notfound():
+    """Test get user with id out of range."""
+    response = client.get("/111111")
+    assert response.status_code == HTTPStatus.NOT_FOUND
