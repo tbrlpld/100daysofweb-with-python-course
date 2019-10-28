114144a1d59857c8610f1a4bb634a4548dbae369
Tibor Leupold <tibor@lpld.io>
2019-10-27 16:46:35 -1000

    Added first tests for user creation

--- work/011-api-star-people/test_app.py
+++ work/011-api-star-people/test_app.py
@@ -43,3 +43,45 @@ def test_get_user_notfound():
     response = client.get("/111111")
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["error"] == "User not found."
+
+
+def test_create_new_user():
+    """Test creation of new user."""
+    initial_user_count = len(client.get("/").json())
+    assert initial_user_count == 1000
+
+    new_user_data = {
+        "userhash": "12345678901234567890123456789012",
+        "username": "newuser",
+        "fullname": "Some Body",
+        "joined": "2019-10-27",
+        "timezone": "Europe/Stockholm",
+    }
+    response = client.post("/", data=new_user_data)
+    assert respone.status_code == HTTPStatus.CREATED
+
+    assert len(client.get("/").json()) == initial_user_count + 1
+    persistent_user = client.get("/1001")
+    assert persistent_user["username"] == new_user_data["username"]
+    assert persistent_user["fullname"] == new_user_data["fullname"]
+    assert persistent_user["joined"] == new_user_data["joined"]
+
+
+def test_create_new_user_with_no_data():
+    """Create new user without passing any data."""
+    empty_data = {}
+    response = client.get("/", data=empty_data)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_create_new_user_with_invalid_data():
+    """Create new user with invalid data."""
+    invalid_data = {
+        "userhash":"123",
+        "username": "x" * 51,
+        "fullname": "A" * 101,
+        "joined": "2001-10-17",
+        "timezone": "Moon/City",
+    }
+    response = client.get("/", data=invalid_data)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
