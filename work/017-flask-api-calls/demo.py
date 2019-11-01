ee2e956366ce5f6dfab751fd18cfaaf17dd1ebb5
Tibor Leupold <tibor@lpld.io>
2019-11-01 16:54:04 -0700

    Copied setup from day 1

---
+++ work/017-flask-api-calls/demo.py
@@ -0,0 +1,5 @@
+from program import app  # this is actually accessing what's in the __init__.py
+
+
+
+
---
+++ work/017-flask-api-calls/program/routes.py
@@ -0,0 +1,16 @@
+from flask import render_template
+
+# Flask is already imported in the __init__.py
+from program import app  # This is in the __init__.py
+# Now I have the app
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html.j2")
+
+
+@app.route("/100days")
+def route100days():
+    return render_template("100days.html.j2")
