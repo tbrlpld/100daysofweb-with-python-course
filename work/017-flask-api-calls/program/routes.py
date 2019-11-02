c65451242ef318c3a504c42542de7d511376baa4
Tibor Leupold <tibor@lpld.io>
2019-11-01 17:16:55 -0700

    Added Chuck Norris joke endpoint

--- work/017-flask-api-calls/program/routes.py
+++ work/017-flask-api-calls/program/routes.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 # Thrid-Party Imports
 from flask import render_template
+import requests
 
 # First-Party Imports
 # Flask is already imported in the __init__.py
@@ -24,3 +25,16 @@ def index():
 @app.route("/100days")
 def route100days():
     return render_template("100days.html.j2")
+
+
+def get_chuck_norris_joke():
+    """Get a random Chuck Norris joke from the API."""
+    response = requests.get("https://api.chucknorris.io/jokes/random")
+    json_data = response.json()
+    return json_data["value"]
+
+
+@app.route("/chuck")
+def chuck():
+    joke = get_chuck_norris_joke()
+    return render_template("chuck.html.j2", joke=joke)
