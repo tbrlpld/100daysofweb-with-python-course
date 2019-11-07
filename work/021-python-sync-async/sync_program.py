7ccbf451cc561a493e1a83be2ffb25011207354f
Tibor Leupold <tibor@lpld.io>
2019-11-06 20:22:29 -0800

    Finished synchronous program

--- work/021-python-sync-async/sync_program.py
+++ work/021-python-sync-async/sync_program.py
@@ -2,9 +2,10 @@
 
 """Synchronous program as example that can be turned into a async program."""
 
-import time
+# Standard Library Imports
 from datetime import datetime
 from random import random
+import time
 
 
 def main():
@@ -13,10 +14,11 @@ def main():
     print("Starting the app. Start time: {0}".format(t0))
 
     data = []
-    generate_data(10, data)
+    generate_data(20, data)
+    processing_data(20, data)
 
     dt = datetime.now() - t0
-    print("Finished run. Run time: {0}".format(dt))
+    print("Finished run. Run time: {0:.2f} s".format(dt.total_seconds()))
 
 
 def generate_data(num: int, data: list):
@@ -27,6 +29,24 @@ def generate_data(num: int, data: list):
 
         print("+++ Generated Data: {0}".format(value), flush=True)
         time.sleep(random() + 0.5)  # noqa: S311
+
+
+def processing_data(num: int, data: list):
+    """Process data in the (mutable) list."""
+    processed = 0
+    while processed < num:
+        item = data.pop(0)
+        value = item[0]
+        creation_time = item[1]
+        tunaround_time = datetime.now() - creation_time
+        processed += 1
+
+        print("--- Processing Data: {0} --- Turnaround time: {1}".format(
+            value,
+            tunaround_time,
+        ))
+
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
