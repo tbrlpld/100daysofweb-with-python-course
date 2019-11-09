af4c980c4607355bc96a6fbbba7c7a663029daa4
Tibor Leupold <tibor@lpld.io>
2019-11-08 19:43:18 -1000

    Converted flask to quart

--- work/023-flask2quart/cityscape_quart_async/views/city_api.py
+++ work/023-flask2quart/cityscape_quart_async/views/city_api.py
@@ -1,7 +1,7 @@
-import flask
+import quart
 from services import weather_service, sun_service, location_service
 
-blueprint = flask.blueprints.Blueprint(__name__, __name__)
+blueprint = quart.blueprints.Blueprint(__name__, __name__)
 
 
 @blueprint.route('/api/events/<city>/<state>/<country>', methods=['GET'])
@@ -13,16 +13,16 @@ def events(city: str, state: str, country: str):
         "country": country,
     }
     if not player:
-        flask.abort(404)
-    return flask.jsonify(player)
+        quart.abort(404)
+    return quart.jsonify(player)
 
 
 @blueprint.route('/api/weather/<zip_code>/<country>', methods=['GET'])
 def weather(zip_code: str, country: str):
     weather_data = weather_service.get_current(zip_code, country)
     if not weather_data:
-        flask.abort(404)
-    return flask.jsonify(weather_data)
+        quart.abort(404)
+    return quart.jsonify(weather_data)
 
 
 @blueprint.route('/api/sun/<zip_code>/<country>', methods=['GET'])
@@ -30,5 +30,5 @@ def sun(zip_code: str, country: str):
     lat, long = location_service.get_lat_long(zip_code, country)
     sun_data = sun_service.for_today(lat, long)
     if not sun_data:
-        flask.abort(404)
-    return flask.jsonify(sun_data)
+        quart.abort(404)
+    return quart.jsonify(sun_data)
