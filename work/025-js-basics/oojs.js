2e1dc60162c826db40179de209f05ec3a56d989a
Tibor Leupold <tibor@lpld.io>
2019-11-10 20:15:22 -1000

    Extending prototype functionality

--- work/025-js-basics/oojs.js
+++ work/025-js-basics/oojs.js
@@ -1,19 +1,21 @@
-// const person = {
-//   name: {
-//     first: "Bob",
-//     last: "Smith",
-//   },
-//   age: 32,
-//   gender: "male",
-//   interests: ["music", "skiing"],
-//   bio: function() {
-//     return this.name[0] + " " + this.name[1] + " is " + this.age + " years old. He likes " + this.interests[0] + " and " + this.interests[1] + ".";
-//   },
-//   greeting: function() {
-//     return "Hi, I'm " + this.name[0] + ".";
-//   },
-// };
+// Direct object creation
+const person = {
+  name: {
+    first: "Bob",
+    last: "Smith",
+  },
+  age: 32,
+  gender: "male",
+  interests: ["music", "skiing"],
+  bio: function() {
+    return this.name[0] + " " + this.name[1] + " is " + this.age + " years old. He likes " + this.interests[0] + " and " + this.interests[1] + ".";
+  },
+  greeting: function() {
+    return "Hi, I'm " + this.name[0] + ".";
+  },
+};
 
+// Object creation with constructor function
 function Person(name) {
   this.name = name;
   this.greeting = function () {
@@ -50,3 +52,11 @@ console.log(person1.name);
 const person3 = new slava.constructor("Peter")
 console.log(person3.name);
 console.log(person3.greeting());
+
+// Adding functionality to prototype
+Person.prototype.farewell = function() {
+  return this.name + " has left."
+};
+console.log(slava.farewell())
+console.log(person3.farewell())
+
