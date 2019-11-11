1088bb0b062035160be243201f5e38ea8902d820
Tibor Leupold <tibor@lpld.io>
2019-11-10 21:28:35 -1000

    Added getter and setter for Teacher subject

--- work/025-js-basics/ooECMA2015.js
+++ work/025-js-basics/ooECMA2015.js
@@ -5,11 +5,11 @@ class Person {
   };
 
   greeting() {
-    return `Hi, I'am ${this.name}`
+    return `Hi, I'am ${this.name}`;
   };
 
   farewell() {
-    return `${this.name} has left`
+    return `${this.name} has left`;
   };
 }
 
@@ -20,7 +20,15 @@ console.log(person1.greeting());
 class Teacher extends Person {
   constructor(name, age, subject) {
     super(name, age);
-    this.subject = subject;
+    this._subject = subject;
+  }
+
+  get subject() {
+    return this._subject;
+  }
+
+  set subject(newSubject) {
+    this._subject = newSubject;
   }
 }
 
@@ -28,3 +36,6 @@ const teacher1 = new Teacher("Ms. Alice", 32, "Biology");
 console.log(teacher1.name);
 console.log(teacher1.subject);
 console.log(teacher1.farewell());
+
+teacher1.subject = "Geography"
+console.log(teacher1.subject);
