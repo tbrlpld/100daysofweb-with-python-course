84f54e9f6aed2df147959585c377862c59a03e8a
Tibor Leupold <tibor@lpld.io>
2019-11-10 21:25:18 -1000

    Derived Teacher class from Person class

--- work/025-js-basics/ooECMA2015.js
+++ work/025-js-basics/ooECMA2015.js
@@ -16,3 +16,15 @@ class Person {
 const person1 = new Person("Alice", 32);
 console.log(person1.name);
 console.log(person1.greeting());
+
+class Teacher extends Person {
+  constructor(name, age, subject) {
+    super(name, age);
+    this.subject = subject;
+  }
+}
+
+const teacher1 = new Teacher("Ms. Alice", 32, "Biology");
+console.log(teacher1.name);
+console.log(teacher1.subject);
+console.log(teacher1.farewell());
