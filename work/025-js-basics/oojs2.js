5643fbc2b60969029a51257a3130e3c577fce92b
Tibor Leupold <tibor@lpld.io>
2019-11-10 21:15:56 -1000

    Derinving a Teacher constructor from Person

--- work/025-js-basics/oojs2.js
+++ work/025-js-basics/oojs2.js
@@ -8,6 +8,9 @@ function Person(name, age) {
 Person.prototype.greeting = function() {
   return "Hi, I'm " + this.name;
 };
+Person.prototype.farewell = function() {
+  return this.name + " has left."
+};
 
 const person1 = new Person("Alice", 32);
 console.log(person1.greeting())
@@ -15,11 +18,14 @@ console.log(person1.greeting())
 
 // Defining a derived constructor
 function Teacher(name, age, subject) {
-  Person.call(this, name, age)
+  Person.call(this, name, age);
 
-  this.subject = subject
+  this.subject = subject;
 }
+// Adding the Person.prototype methods to the Teacher
+Teacher.prototype = Object.create(Person.prototype);
 
 const teacher1 = new Teacher("Ms. Alice", 32, "Biology");
-console.log(teacher1.name)
-console.log(teacher1.subject)
+console.log(teacher1.name);
+console.log(teacher1.subject);
+console.log(teacher1.greeting());
