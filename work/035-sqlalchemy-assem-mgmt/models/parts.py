6c00ec3e7efabdc447fff68555a3f0d90ca2d73b
Tibor Leupold <tibor@lpld.io>
2019-11-20 21:17:04 -1000

    Added Part and StockPart models

---
+++ work/035-sqlalchemy-assem-mgmt/models/parts.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*.
+
+"""Defines Part class."""
+
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from models.base import Base
+
+
+class Part(Base):
+    """Simple part class."""
+
+    __tablename__ = "parts"
+
+    id_ = sa.Column("id", sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String(30))  # noqa: WPS432
+
+    stock_part = orm.relation("StockPart", back_populates=True, uselist=False)
+
+
+class StockPart(Base):
+    """Part that is in stock. Can have 0 stock for out-of-stock."""
+
+    __tablename__ = "inventory"
+
+    id_ = sa.Column("id", sa.Integer, primary_key=True, autoincrement=True)
+
+    part_id = sa.Column(sa.ForeignKey("parts.id"), nullable=False)
+    part = orm.relation("Part", back_populates=True, uselist=False)
+
+    count = sa.Column(sa.Integer, default=0)
