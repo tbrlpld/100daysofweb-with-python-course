ebf7e733a35aa4c42d280db113ed24b2ae754d5f
Tibor Leupold <tibor@lpld.io>
2019-11-20 19:32:13 -1000

    Created minimal intialization

--- work/035-sqlalchemy-assem-mgmt/program.py
+++ work/035-sqlalchemy-assem-mgmt/program.py
@@ -12,7 +12,10 @@ def main():
     # Config DB
     current_dir = os.path.dirname(os.path.abspath(__file__))
     db_filename = "assem_mgmt.sqlite"
-    connection_string = "sqlite:///" + os.path.join(current_dir, db_filename)
+    db_path = os.path.join(current_dir, db_filename)
+    if os.exists(db_path):
+        os.remove(db_path)
+    connection_string = "sqlite:///" + db_path
 
     # DB engine
     engine = sa.create_engine(connection_string)
