0f9063a873e8228039988abc0a7a0f43032e0775
Tibor Leupold <tibor@lpld.io>
2019-11-21 18:47:34 -1000

    Added ui function to add new part

--- work/035-sqlalchemy-assem-mgmt/ui.py
+++ work/035-sqlalchemy-assem-mgmt/ui.py
@@ -3,8 +3,9 @@
 """Defines components and functions for the user interface."""
 
 import sys
-
 from typing import Dict, Callable, Tuple
+
+from data import services
 
 
 def loop():
@@ -18,20 +19,34 @@ def loop():
 def prompt_function_select():
     """Prompt user to select interactive function. Execute function."""
     FUNCTIONS: Dict[str, Tuple(str, Callable)] = {
+        "a": ("(a)dd part", add_part),
         "x": ("e(x)it program", end_program),
     }
     options_string: str = ""
     for key in FUNCTIONS:
-        options_string = "{0}, ".format(FUNCTIONS[key][0])
+        options_string += "{0}, ".format(FUNCTIONS[key][0])
     options_string = options_string[:-2]
 
-    user_input: str = input("Select option: {0}\n>>> ".format(options_string))
+    user_input: str = input("\nSelect option: {0}\n>>> ".format(options_string))
     if user_input not in FUNCTIONS.keys():
         print("'{0}' is not a valid option.\n".format(user_input))
         return
     else:
         selected_function: Callable = FUNCTIONS[user_input][1]
         selected_function()
+
+
+def add_part():
+    part_name: str = input("Name for new part:\n>>> ")
+    part_count_str: str = input("Count of new part in inventory:\n>>> ")
+
+    try:
+        part_count: int = int(part_count_str)
+    except ValueError:
+        print("Count can only be integer!")
+        return
+
+    services.create_new_stock_part(name=part_name, count=part_count)
 
 
 def end_program():
