d4f0485bfb48c09d02ac9ca4afa50ed3ae15bfe7
Tibor Leupold <tibor@lpld.io>
2019-11-23 13:39:44 -1000

    Loading starter data on app start

--- work/037-pyramid-intro/billtracker/billtracker/__init__.py
+++ work/037-pyramid-intro/billtracker/billtracker/__init__.py
@@ -1,11 +1,24 @@
+# -*- conding: utf-8 -*-
+
+"""App entry point."""
+
+import os
+
 from pyramid.config import Configurator
+
+from billtracker.bin.load_base_data import load_starter_data
+from billtracker.data.db_session import DbSession
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    """This function returns a Pyramid WSGI application."""
     with Configurator(settings=settings) as config:
-        config.include('pyramid_chameleon')
-        config.include('.routes')
+        config.include("pyramid_chameleon")
+        config.include(".routes")
         config.scan()
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    db_file = os.path.join(current_dir, "db/billtracker.sqlite3")
+    DbSession.global_init(db_file)
+    load_starter_data()
     return config.make_wsgi_app()
