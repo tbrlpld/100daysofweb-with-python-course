00f863f65ea11e3203c12f4fbb31079e8c3b68c0
Tibor Leupold <tibor@lpld.io>
2019-11-23 14:57:34 -1000

    Showing databse data on langing page

--- work/037-pyramid-intro/billtracker/billtracker/views/home.py
+++ work/037-pyramid-intro/billtracker/billtracker/views/home.py
@@ -2,19 +2,21 @@
 
 """Defines view functions for the billtracker app."""
 
+from typing import Optional, Dict
+
 from pyramid.view import view_config
+
+from billtracker.data import repository
+from billtracker.data.models.users import User
 
 
 @view_config(route_name="home", renderer="../templates/home.pt")
-def home(request):
+def home(request) -> Dict:
     """Render home template with given project name."""
     # noqa: DAR101, DAR201
+    user_id: int = 1
+    user: Optional[User] = repository.get_user_by_id(user_id)
+
     return {
-        "project": "Bill Tracker",
-        "items": [
-            "item1",
-            "item2",
-            "item3",
-            "item4",
-        ],
+        "user": user,
     }
