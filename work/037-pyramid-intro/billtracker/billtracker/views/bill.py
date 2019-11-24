ab1ecebaca6f6fc9902be537add88399b9b07a12
Tibor Leupold <tibor@lpld.io>
2019-11-23 16:48:35 -1000

    Added error handling for bill view

--- work/037-pyramid-intro/billtracker/billtracker/views/bill.py
+++ work/037-pyramid-intro/billtracker/billtracker/views/bill.py
@@ -4,8 +4,9 @@
 
 from typing import Optional, Dict
 
-from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid.request import Request
+from pyramid.view import view_config
 
 from billtracker.data import repository
 from billtracker.data.models.users import User
@@ -23,8 +24,14 @@ def bill_detail_get(request: Request) -> Dict:
     user_id: int = 1
     user: Optional[User] = repository.get_user_by_id(user_id)
 
-    bill_id: int = int(request.matchdict.get("bill_id"))
-    bill: Optional[Bill] = repository.get_bill_by_id(bill_id)
+    try:
+        bill_id: int = int(request.matchdict.get("bill_id"))
+    except ValueError:
+        raise HTTPNotFound
+    else:
+        bill: Optional[Bill] = repository.get_bill_by_id(bill_id)
+        if bill is None:
+            raise HTTPNotFound
 
     return {
         "bill": bill,
