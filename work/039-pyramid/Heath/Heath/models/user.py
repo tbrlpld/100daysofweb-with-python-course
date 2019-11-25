2aaf8502038cede136113a1e262dd12edf80bb28
Tibor Leupold <tibor@lpld.io>
2019-11-24 16:17:31 -1000

    Added user model

---
+++ work/039-pyramid/Heath/Heath/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Text,
+    DateTime,
+)
+
+from .meta import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id_ = Column("id", Integer, primary_key=True)
+    name = Column(Text, index=True)
+    email = Column(Text, index=True, unique=True)
+    password = Column(Text)
+    created = Column(DateTime)
