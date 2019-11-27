bc3979580ea3e2f23a72f8f8c48977161322708c
Tibor Leupold <tibor@lpld.io>
2019-11-27 14:11:50 -0800

    Loading todos from external resource

--- work/042-react-todo/todo/src/App.js
+++ work/042-react-todo/todo/src/App.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require("axios")
+
+const TODO_ENDPOINT = "https://jsonplaceholder.typicode.com/todos"
 
 function Todo(props) {
   return (
@@ -14,12 +17,35 @@ function Todo(props) {
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      allTodos: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get(TODO_ENDPOINT)
+      .then(response => {
+        this.setState({
+          allTodos: response.data,
+        })
+        // console.log(this.state.allTodos)
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }
+
   render () {
     return (
       <div className="App">
         <header className="">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <span className="navbar-brand">React Todo App</span>
+            <span className="navbar-brand">
+              <img src={logo} alt="React Logo" width="32px"/>
+              React Todo App
+            </span>
           </nav>
         </header>
         <div className="container">
