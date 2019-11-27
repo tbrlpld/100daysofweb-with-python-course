6c954a6a558c13a9d027dc1112539ae7f7796bae
Tibor Leupold <tibor@lpld.io>
2019-11-26 21:19:08 -0800

    Handling forbidden requests

--- work/039-pyramid/Heath/Heath/views/auth.py
+++ work/039-pyramid/Heath/Heath/views/auth.py
@@ -2,7 +2,7 @@
 
 """Defines authentication view for login / logout."""
 
-from pyramid.view import view_config
+from pyramid.view import view_config, forbidden_view_config
 from pyramid.httpexceptions import HTTPFound
 from pyramid.security import remember, forget
 
@@ -50,3 +50,16 @@ def logout(request):
     headers = forget(request)
     next_url = request.route_url("landing")
     return HTTPFound(location=next_url, headers=headers)
+
+
+@forbidden_view_config()
+def forbidden_view(request):
+    """Redirect when a resource is not to be available."""
+    if request.user is not None:
+        # Logged-in users are redirected to their home.
+        next_url = request.route_url("home")
+    else:
+        # Not logged-in users are redirected to the login page.
+        # The url they where forbidden from seeing is passed as next parameter.
+        next_url = request.route_url("login", _query={"next": request.url})
+    return HTTPFound(location=next_url)
