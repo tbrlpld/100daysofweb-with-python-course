995fc7bb80e3f7cb4ebe1034e6aeb2c84c3fa07f
Tibor Leupold <tibor@lpld.io>
2019-11-30 15:54:51 -0800

    Created game area with css

--- work/044-react-tictactoe/tictactoe/src/App.js
+++ work/044-react-tictactoe/tictactoe/src/App.js
@@ -1,25 +1,36 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+const GAME_TITLE = "tic-tac-toe"
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h1>{ GAME_TITLE }</h1>
+        </div>
+        <div className="game-wrapper">
+          <div className="game">
+            <div className="field-row">
+              <div className="field"></div>
+              <div className="field"></div>
+              <div className="field"></div>
+            </div>
+            <div className="field-row">
+              <div className="field"></div>
+              <div className="field"></div>
+              <div className="field"></div>
+            </div>
+            <div className="field-row">
+              <div className="field"></div>
+              <div className="field"></div>
+              <div className="field"></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
