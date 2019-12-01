6bef21db0b4b6818c38148968fed0c2b68c620fe
Tibor Leupold <tibor@lpld.io>
2019-11-30 16:55:57 -0800

    Drawing game dynamically.

--- work/044-react-tictactoe/tictactoe/src/App.js
+++ work/044-react-tictactoe/tictactoe/src/App.js
@@ -3,9 +3,54 @@ import './App.css';
 
 
 const GAME_TITLE = "tic-tac-toe"
-
+const PLAYER_SYMBOLS = ["X", "O"]
+const INITIAL_STATE = {
+  activePlayer: 1,
+  gameStatus: [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+  ],
+}
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = INITIAL_STATE;
+  }
+
+  componentDidMount() {
+    console.log("Did mount.")
+    // this.resetGame();
+  }
+
+  resetGame = () => {
+    this.setState(INITIAL_STATE);
+    return;
+  }
+
+  drawGame = () => {
+    const rows = this.state.gameStatus.map(this.drawRow);
+    const game = (<div className="game">{ rows }</div>);
+    return game;
+  }
+
+  drawRow = (row, index) => {
+    const rowKey = index;
+    console.log("Drawing Row: " + rowKey);
+    console.log(row);
+    const fields = row.map(this.drawFields);
+    return (<div className="field-row" key={index}>{fields}</div>);
+  }
+
+  drawFields = (field, index) => {
+    const fieldKey = index;
+    console.log("Drawing Field: " + fieldKey);
+    console.log(field);
+    return (<div className="field" key={fieldKey}>{ field }</div>)
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,23 +58,7 @@ class App extends Component {
           <h1>{ GAME_TITLE }</h1>
         </div>
         <div className="game-wrapper">
-          <div className="game">
-            <div className="field-row">
-              <div className="field"></div>
-              <div className="field"></div>
-              <div className="field"></div>
-            </div>
-            <div className="field-row">
-              <div className="field"></div>
-              <div className="field"></div>
-              <div className="field"></div>
-            </div>
-            <div className="field-row">
-              <div className="field"></div>
-              <div className="field"></div>
-              <div className="field"></div>
-            </div>
-          </div>
+          {this.drawGame()}
         </div>
       </div>
     );
