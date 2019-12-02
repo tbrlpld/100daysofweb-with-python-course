b89fa9a990f2ef4de9dba529b1d8dcf61220d703
Tibor Leupold <tibor@lpld.io>
2019-12-02 11:10:43 -0800

    Registered quotes app and project wide template dir

--- work/046-django-quotes/mysite/settings.py
+++ work/046-django-quotes/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quotes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'mysite/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
