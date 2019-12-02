e15e195807fef748def1755a20c7a4807f7d9b16
Tibor Leupold <tibor@lpld.io>
2019-12-02 11:36:57 -0800

    Added quotes model

--- work/046-django-quotes/quotes/models.py
+++ work/046-django-quotes/quotes/models.py
@@ -1,3 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""Defines the django ORM models of the quotes app."""
+
 from django.db import models
 
-# Create your models here.
+
+class Quote(models.Model):
+    """Quote model."""
+
+    quote = models.TextField()
+    author = models.CharField(max_length=100)
+    source = models.URLField()
+    cover = models.URLField()
+    # Only set once when creating
+    added = models.DateTimeField(auto_now_add=True)
+    # Updated on every save.
+    edited = models.DateTimeField(auto_now=True)
+
+    def __repr__(self) -> str:
+        """
+        Return a string to represent the model.
+
+        Returns:
+            str: String that represents the model.
+
+        """
+        return "{quote} - {author}".format(
+            quote=self.quote,
+            author=self.author,
+        )
+
+    class Meta(object):
+        """Add additional settings."""
+
+        # Order quotes by descending "added" date.
+        ordering = ["-added"]
