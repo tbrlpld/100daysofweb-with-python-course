ad196369ea0bf95e8b36bd01c1b4f34b9ef721ff
Tibor Leupold <tibor@lpld.io>
2019-12-03 16:51:27 -0800

    Added routes for quotes CRUD

--- work/046-django-quotes/quotes/views.py
+++ work/046-django-quotes/quotes/views.py
@@ -6,6 +6,42 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    """Return the rendered index page."""
-    return HttpResponse("Welcome to my first custom view.")
+from quotes.models import Quote
+
+# def index(request: HttpRequest) -> HttpResponse:
+#     """Return the rendered index page."""
+#     return HttpResponse("Welcome to my first custom view.")
+
+
+def quote_list(request: HttpRequest) -> HttpResponse:
+    """Render a list of quotes."""  # noqa: 201
+    pass
+    # quotes = Quote.objects.all()
+    # return render(
+    #     request,
+    #     "quotes/quote_list.html",
+    #     {"quotes": quotes},
+    # )
+
+
+def quote_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    """Render a single quote."""  # noqa: 201
+    pass
+
+
+def quote_create(request: HttpRequest) -> HttpResponse:
+    """Create a new quote."""  # noqa: 201
+    pass
+
+
+def quote_update(request: HttpRequest, pk: int) -> HttpResponse:
+    """Update a quote."""  # noqa: 201
+    pass
+
+
+def quote_delete(request: HttpRequest, pk: int) -> HttpResponse:
+    """Delete a quote."""  # noqa: 201
+    pass
+
+
+
