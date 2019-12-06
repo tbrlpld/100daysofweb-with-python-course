a80d3cbf12f33c3b42196d1498a5a29db010e5fe
Tibor Leupold <tibor@lpld.io>
2019-12-05 16:15:48 -0800

    Added tests for a books detail page

--- work/049-selenium/test_pbreadinglist.py
+++ work/049-selenium/test_pbreadinglist.py
@@ -50,8 +50,8 @@ def driver_login():
 
 
 def test_homepage_title(driver_home):
-    expected = "PyBites My Reading List | Because We Love Books"
-    assert driver_home.title == expected
+    expected_title = "PyBites My Reading List | Because We Love Books"
+    assert driver_home.title == expected_title
 
 
 def test_number_of_thumbs_homepage(driver_home):
@@ -62,32 +62,58 @@ def test_number_of_thumbs_homepage(driver_home):
 
 def test_has_login_link(driver_home):
     try:
-        login_link = driver_home.find_element_by_link_text("Login")
+        driver_home.find_element_by_link_text("Login")
     except NoSuchElementException:
         pytest.fail("There should be a login button on the site.")
 
 
-def _get_number_books_read(driver):
-    pass
-
-
 def test_book_page_title(driver_first_book):
-    pass
+    expected_title = (
+        "PyBites My Reading List | The Hitchhiker's Guide to Python"
+    )
+    assert driver_first_book.title == expected_title
 
 
 def test_book_page_meta_data(driver_first_book):
-    pass
+    expected_authors = "Kenneth Reitz, Tanya Schlusser"
+    expected_publisher = "O'Reilly Media, Inc."
+    expected_date = "2016-08-30"
+
+    page_source = driver_first_book.page_source
+    assert expected_authors in page_source
+    assert expected_publisher in page_source
+    assert expected_date in page_source
 
 
 def test_book_page_has_add_book_link(driver_first_book):
-    pass
+    try:
+        driver_first_book.find_element_by_link_text("Add Book")
+    except NoSuchElementException:
+        pytest.fail("There should be an 'Add Book' link on the site.")
 
 
 def test_search_box_auto_direct(driver_first_book):
-    pass
+    search_term = "Fluent Python"
+    search_box = driver_first_book.find_element_by_name("searchTitles")
+    search_box.send_keys(search_term)
+    sleep(2)
+
+    results = driver_first_book.find_elements_by_css_selector(
+        "div.ac_results > ul > li",
+    )
+    first_result = results[0]
+    first_result.click()
+
+    expected_directed_page_title = "PyBites My Reading List | Fluent Python"
+    assert driver_first_book.title == expected_directed_page_title
+
 
 
 def test_login_to_site(driver_login):
+    pass
+
+
+def _get_number_books_read(driver):
     pass
 
 
