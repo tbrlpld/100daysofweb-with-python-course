8187248e50e9f13c1183f7d7386a0ec2cd0800d4
Tibor Leupold <tibor@lpld.io>
2019-12-06 16:01:20 -0800

    Added api endpoints

--- work/050-responder/app.py
+++ work/050-responder/app.py
@@ -16,5 +16,42 @@ def index(_, response: responder.Response) -> None:
     response.html = api.template("/index.html", recreator="Tibor")
 
 
+# Search movies
+# GET /api/search/{keyword}
+@api.route("/api/search/{keyword}")
+def search_by_keyword(_, response: responder.Response, keyword: str) -> None:
+    response.media = {"searched": keyword}
+
+# Movies by director
+# GET /api/director/{director_name}
+@api.route("/api/director/{director_name}")
+def search_by_director(_, response: responder.Response, director_name: str) -> None:
+    response.media = {"searched": director_name}
+
+# Movie by IMDB code
+# GET /api/movie/{imdb_number}
+@api.route("/api/movie/{imdb_number}")
+def search_by_keyword(_, response: responder.Response, imdb_number: str) -> None:
+    response.media = {"searched": imdb_number}
+
+# Top 10 Movies (by IMDB score)
+# GET /api/movie/top
+@api.route("/api/movie/top")
+def search_by_keyword(_, response: responder.Response) -> None:
+    response.media = {"searched": "Top 10 Movies"}
+
+# All genres
+# GET /api/movie/genre/all
+@api.route("/api/movie/genre/all")
+def search_by_keyword(_, response: responder.Response) -> None:
+    response.media = {"Genres": "All"}
+
+# Top movies for a given genres
+# GET /api/movie/genre/{genre}
+@api.route("/api/movie/genre/{genre}")
+def search_by_keyword(_, response: responder.Response, genre: str) -> None:
+    response.media = {"searched": genre}
+
+
 if __name__ == "__main__":
     main()
