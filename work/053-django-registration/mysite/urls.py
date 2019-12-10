3a917af33f8d4e6b76fbfd1d434e53fd93bef5ae
Tibor Leupold <tibor@lpld.io>
2019-12-10 15:39:15 -0800

    Use two step register process

--- work/053-django-registration/mysite/urls.py
+++ work/053-django-registration/mysite/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('quotes.urls')),
     path('my-backend/', admin.site.urls),
-    path(r'accounts/', include('django_registration.backends.one_step.urls')),
+    path(r'accounts/', include('django_registration.backends.activation.urls')),
     path(r'accounts/', include('django.contrib.auth.urls')),
 ]
