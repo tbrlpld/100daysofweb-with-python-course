eb6ff0d4f50a1d260e5243d4f2b952327a0d603c
Tibor Leupold <tibor@lpld.io>
2019-12-27 12:33:08 -1000

    Add authentication links to API frontend

--- work/070-django-rest/api/urls.py
+++ work/070-django-rest/api/urls.py
@@ -3,7 +3,7 @@
 """URLs for the Quotes API."""
 
 
-from django.urls import path
+from django.urls import path, include
 
 from api.views import QuotesListView, QuoteRUDView
 
@@ -11,4 +11,5 @@ from api.views import QuotesListView, QuoteRUDView
 urlpatterns = [
     path("", QuotesListView.as_view()),
     path("<int:pk>", QuoteRUDView.as_view()),
+    path("api-auth/", include("rest_framework.urls")),
 ]
