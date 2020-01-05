6f01d571330847e533135be584b579c1768e01cf
Tibor Leupold <tibor@lpld.io>
2020-01-04 20:19:08 -1000

    Loop over paragraphs after day heading

--- work/078-twitter-bot/logtweet.py
+++ work/078-twitter-bot/logtweet.py
@@ -4,6 +4,7 @@
 
 from datetime import date, datetime, timedelta
 import re
+import sys
 
 import requests
 import bs4
@@ -15,6 +16,8 @@ URL = "https://log100days.lpld.io/log.md"
 TODAY = date.today() - timedelta(days=10)
 
 DATE_FORMAT = "%B %d, %Y"
+
+MAX_TWEET_LEN = 240
 
 
 def is_today(day_heading_text: str) -> bool:
@@ -45,8 +48,44 @@ if __name__ == "__main__":
     soup = bs4.BeautifulSoup(response.text, "html.parser")
     day_headings = soup.find_all("h2")
 
+    # Get today's heading
+    today_heading = None
     for day in day_headings:
         if is_today(day.text):
-            print(day)
+            today_heading = day
+    if today_heading is None:
+        print("No log for today found!")
+        sys.exit(1)
+
+    # Grab today's subheadings
+    content_heading = None
+    today_subheadings = day.find_next_siblings("h3")
+    for subhead in today_subheadings:
+        if subhead.text == "Today's Progress":
+            content_heading = subhead
+    if content_heading is None:
+        print("No content found for today!")
+        sys.exit(1)
+
+    # Loop over the next siblings until you find something
+    # that is not a paragraph.
+    current_element = content_heading
+    while True:
+        print(current_element)
+        # possible_content = content
+        next_sibling = current_element.find_next_sibling()
+        if next_sibling.name != "p":
+            # Leave loop if not a paragraph.
+            break
+        current_element = next_sibling
+        # possible_content += current_element.text
+
+        # if len(possible_content) > MAX_TWEET_LEN:
+        #     break
+        # content = possible_content
+
+    # print(content)
+
+
 
 
