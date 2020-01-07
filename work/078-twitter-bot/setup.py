e41021b32bd117ef46d8d0ee4ca740434587d8b3
Tibor Leupold <tibor@lpld.io>
2020-01-06 22:54:16 -1000

    Prepare package for distribution on PyPi

--- work/078-twitter-bot/setup.py
+++ work/078-twitter-bot/setup.py
@@ -13,17 +13,27 @@ requires = [
     "tweepy",
 ]
 
+develop_requires = [
+    "wheel",
+    # "twine" to upload to pypi in secure way.
+    # See: https://packaging.python.org/tutorials/packaging-projects/
+    "twine",
+]
+
 setup(
-    name="LogTweet",
+    name="logtweet",
     version="0.1.0",
     author="Tibor Leupold",
     author_email="tibor@lpld.io",
-    description="Create a tweet based on the log message for today from https://log100days.lpld.io/log.md",
+    description="Create a tweet based on a #100DaysOfCode log message",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tbrlpld/100daysofweb-with-python-course/tree/master/work/078-twitter-bot",
     python_requires=">=3.6",
     install_requires=requires,
+    extras_require={
+        "develop": develop_requires,
+    },
     packages=["logtweet"],
     entry_points={
         "console_scripts": [
