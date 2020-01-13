67ef968183cbe4544cfc488951cd7d696d76e9b1
Tibor Leupold <tibor@lpld.io>
2020-01-12 17:30:08 -1000

    Add subdomain bounce

--- work/086-url-shortener/short.py
+++ work/086-url-shortener/short.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 
 
 app = Flask(__name__)
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello from Lambda!"
+
+@app.route("/<shortlink>")
+def redirect_to_long(shortlink):
+    return f"You requested the shortlink: {shortlink}"
 
 
 if __name__ == "__main__":
