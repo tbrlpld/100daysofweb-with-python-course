7f1adf36698a826a1522dcc486448c6fee89ee6f
Tibor Leupold <tibor@lpld.io>
2020-01-12 17:32:12 -1000

    Add redirect to exmaple.com

--- work/086-url-shortener/short.py
+++ work/086-url-shortener/short.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def hello():
     return "Hello from Lambda!"
 
+
+@app.route("/x")
+def exmple():
+    return redirect("http://example.com")
+
 @app.route("/<shortlink>")
 def redirect_to_long(shortlink):
     return f"You requested the shortlink: {shortlink}"
